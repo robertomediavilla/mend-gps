0983a198494b3d32c7a8d25a7b766356090887c8
robertomediavilla <roberto.mediavilla@uam.es>
2026-06-23 12:03:35 +0200

    clean renv and setup files to reduce number of packages

--- src/setup.R
+++ src/setup.R
@@ -2,35 +2,24 @@
 
 library(flextable)
 library(gtsummary)
-library(gt)
-library(fs) # dates and times in outputs
 
 # analysis
 
-library(emmeans)
-library(meta)
 library(survey)
 library(WeightIt)
 library(lme4)
-
-# bootstrap
-
-library(furrr)
-library(future)
+library(sandwich)
 
 # graphics
 
 library(ggrepel)
 library(ggpubr)
-library(sf)
-library(patchwork)
 
 # missing data & mice
 
 library(mice)
 library(VIM)
 library(naniar)
-library(sandwich)
 
 # mediation
 
@@ -39,6 +28,7 @@ library(mediation)
 # grammar
 
 library(broom.mixed)
+library(janitor)
 library(glue)
 library(sjlabelled)
 library(rlang)
